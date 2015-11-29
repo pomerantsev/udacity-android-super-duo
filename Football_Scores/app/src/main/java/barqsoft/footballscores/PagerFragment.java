5324721e3ee251bbde91fb59bc90836b35675a7e
Pavel Pomerantsev <pomerantsevp@gmail.com>
2015-11-29 21:08:06 +0300

    Change view pager direction based on layout direction

--- Football_Scores/app/src/main/java/barqsoft/footballscores/PagerFragment.java
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/PagerFragment.java
@@ -1,6 +1,8 @@
 package barqsoft.footballscores;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -23,7 +25,7 @@ public class PagerFragment extends Fragment
     public static final int NUM_PAGES = 5;
     public ViewPager mPagerHandler;
     private myPageAdapter mPagerAdapter;
-    private MainScreenFragment[] viewFragments = new MainScreenFragment[5];
+    private MainScreenFragment[] viewFragments = new MainScreenFragment[NUM_PAGES];
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
@@ -32,7 +34,7 @@ public class PagerFragment extends Fragment
         mPagerAdapter = new myPageAdapter(getChildFragmentManager());
         for (int i = 0;i < NUM_PAGES;i++)
         {
-            Date fragmentdate = new Date(System.currentTimeMillis()+((i-2)*86400000));
+            Date fragmentdate = new Date(getTime(i));
             SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
             viewFragments[i] = new MainScreenFragment();
             viewFragments[i].setFragmentDate(mformat.format(fragmentdate));
@@ -63,7 +65,7 @@ public class PagerFragment extends Fragment
         @Override
         public CharSequence getPageTitle(int position)
         {
-            return getDayName(getActivity(),System.currentTimeMillis()+((position-2)*86400000));
+            return getDayName(getActivity(),getTime(position));
         }
         public String getDayName(Context context, long dateInMillis) {
             // If the date is today, return the localized version of "Today" instead of the actual
@@ -91,5 +93,14 @@ public class PagerFragment extends Fragment
                 return dayFormat.format(dateInMillis);
             }
         }
+    }
+
+    private long getTime(int position) {
+        Configuration config = getResources().getConfiguration();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 &&
+                config.getLayoutDirection() != View.LAYOUT_DIRECTION_LTR) {
+            position = NUM_PAGES - 1 - position;
+        }
+        return System.currentTimeMillis()+((position-2)*86400000);
     }
 }
