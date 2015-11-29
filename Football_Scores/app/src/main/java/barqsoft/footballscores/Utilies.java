633844f86b50176a9bc668f3f9ecb7141010efe5
Pavel Pomerantsev <pomerantsevp@gmail.com>
2015-11-29 22:58:11 +0300

    Fix league ids

--- Football_Scores/app/src/main/java/barqsoft/footballscores/Utilies.java
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/Utilies.java
@@ -5,52 +5,26 @@ package barqsoft.footballscores;
  */
 public class Utilies
 {
-    public static final int SERIE_A = 357;
-    public static final int PREMIER_LEGAUE = 354;
-    public static final int CHAMPIONS_LEAGUE = 362;
-    public static final int PRIMERA_DIVISION = 358;
-    public static final int BUNDESLIGA = 351;
+    public static final int SERIE_A = 401;
+    public static final int PREMIER_LEAGUE = 398;
+    public static final int PRIMERA_DIVISION = 399;
+    public static final int BUNDESLIGA1 = 394;
+    public static final int BUNDESLIGA2 = 395;
     public static String getLeague(int league_num)
     {
         switch (league_num)
         {
             case SERIE_A : return "Seria A";
-            case PREMIER_LEGAUE : return "Premier League";
-            case CHAMPIONS_LEAGUE : return "UEFA Champions League";
+            case PREMIER_LEAGUE : return "Premier League";
             case PRIMERA_DIVISION : return "Primera Division";
-            case BUNDESLIGA : return "Bundesliga";
+            case BUNDESLIGA1 :
+            case BUNDESLIGA2 : return "Bundesliga";
             default: return "Not known League Please report";
         }
     }
     public static String getMatchDay(int match_day,int league_num)
     {
-        if(league_num == CHAMPIONS_LEAGUE)
-        {
-            if (match_day <= 6)
-            {
-                return "Group Stages, Matchday : 6";
-            }
-            else if(match_day == 7 || match_day == 8)
-            {
-                return "First Knockout round";
-            }
-            else if(match_day == 9 || match_day == 10)
-            {
-                return "QuarterFinal";
-            }
-            else if(match_day == 11 || match_day == 12)
-            {
-                return "SemiFinal";
-            }
-            else
-            {
-                return "Final";
-            }
-        }
-        else
-        {
-            return "Matchday : " + String.valueOf(match_day);
-        }
+        return "Matchday : " + String.valueOf(match_day);
     }
 
     public static String getScores(int home_goals,int awaygoals)
